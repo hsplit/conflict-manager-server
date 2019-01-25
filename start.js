c857aff541e6f0c3d11ae365732cf2a9da2575f3
Oleg Sak <oleg.sak.by@gmail.com>
2019-01-25 22:45:44 +0300

    add 404

--- start.js
+++ start.js
@@ -29,6 +29,13 @@ app.post('/checkfileforday', jsonParser, api.post.checkFileForDay)
 app.post('/checkusersforday', jsonParser, api.post.checkUsersForDay)
 app.post('/getconflictsforday', jsonParser, api.post.getConflictsForDay)
 
+// 404
+app.use((request, response) => response.send(
+  '404 There may be funny cats, ' +
+  'but I\'m tired, I\'m sorry, ' +
+  'but you shouldn\'t lose hope, maybe they will appear soon. Have a nice day!'
+))
+
 // Start
 const startServer = _port => {
   app.listen(_port, () => console.log(`Start on http://localhost:${_port}/`) || opn(`http://localhost:${_port}/`))
